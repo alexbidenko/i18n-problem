f7a54fc9de7df8fe2f52a0b50d748f2c7f103beb
Alexander Bidenko <alexbidenko1998@gmail.com>
2023-12-03 11:37:23 +0300

    first commit

---
+++ components/HelloLang.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const {t} = useI18n();
+</script>
+
+<template>
+  <span>{{ t('lang') }}</span>
+</template>
+
+<i18n locale="en">
+{
+  "lang": "English"
+}
+</i18n>
+
+<i18n locale="de">
+{
+  "lang": "Deutsch"
+}
+</i18n>
